6d67662235a129ac6559ada41f961a830ecdd000
valyy151 <marin.valenta1@gmail.com>
2024-03-17 13:02:34 +0100

    optimistic update on sidebar

--- src/components/boss-link.tsx
+++ src/components/boss-link.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useParams } from "next/navigation";
+import { useOptimistic } from "react";
 
 export function BossLink({
   boss,
@@ -10,11 +11,13 @@ export function BossLink({
   raidId: string;
 }) {
   const params = useParams();
+  const [isClicked, setIsClicked] = useOptimistic(false);
   return (
     <Link
       key={boss.id}
       href={`/${raidId}/${boss.id}`}
-      className={`h-fit w-full rounded-lg px-4 py-2 text-lg font-medium duration-100 hover:bg-gray-800 ${params.boss === boss.id && "bg-gray-800 text-green-400"}`}
+      className={`h-fit w-full rounded-lg px-4 py-2 text-lg font-medium duration-100 hover:bg-gray-800 ${params.boss === boss.id && "bg-gray-800 text-green-400"} ${isClicked && "bg-gray-800 text-green-400"}`}
+      onClick={() => setIsClicked(true)}
     >
       {boss.name}
     </Link>
