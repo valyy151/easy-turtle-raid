34274c49bd63a683db6e0ac2ae9f789edbd47dfc
valyy151 <marin.valenta1@gmail.com>
2024-03-17 14:49:20 +0100

    removed sidebar skeleton

--- src/components/sidebar-skeleton.tsx
+++
@@ -1,7 +0,0 @@
-export function SidebarSkeleton() {
-  return (
-    <div className="w-[22rem] px-4">
-      <div className="mt-4 h-10 animate-pulse rounded-md bg-gray-800"></div>
-    </div>
-  );
-}
