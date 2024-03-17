fb546a91d2d24035311216a970dcab573ead2155
valyy151 <marin.valenta1@gmail.com>
2024-03-17 14:48:14 +0100

    sidebar skeleton width increase

--- src/components/sidebar-skeleton.tsx
+++ src/components/sidebar-skeleton.tsx
@@ -1,6 +1,6 @@
 export function SidebarSkeleton() {
   return (
-    <div className="w-72 px-4">
+    <div className="w-[22rem] px-4">
       <div className="mt-4 h-10 animate-pulse rounded-md bg-gray-800"></div>
     </div>
   );
