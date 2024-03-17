99cf54091742469bcaaf8c0b052f7736f7396534
valyy151 <marin.valenta1@gmail.com>
2024-03-17 15:35:18 +0100

    sidebar ux improvement

--- src/components/boss-link.tsx
+++ src/components/boss-link.tsx
@@ -12,11 +12,13 @@ export function BossLink({
 }) {
   const params = useParams();
   const [isClicked, setIsClicked] = useOptimistic(false);
+
+  const isCurrent = params.boss === boss.id;
   return (
     <Link
       key={boss.id}
       href={`/${raidId}/${boss.id}`}
-      className={`h-fit w-full rounded-lg px-4 py-2 text-lg font-medium duration-100 hover:bg-gray-800 ${params.boss === boss.id && "bg-gray-800 text-green-400"} ${isClicked && "bg-gray-800 text-green-400"}`}
+      className={`h-fit w-full rounded-lg px-4 py-2 text-lg font-medium duration-100 hover:bg-[#111827] ${isCurrent && "bg-[#1f2937] text-green-400 hover:bg-[#1f2937]"}  ${isClicked && "bg-[#1f2937] text-green-400 hover:bg-[#1f2937]"}`}
       onClick={() => setIsClicked(true)}
     >
       {boss.name}
